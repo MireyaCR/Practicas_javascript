6fbce0bb1dd2ea98a537f3a2fb98dd29c1d6f3c0
MireyaCR <mireya.mcr@gmail.com>
2022-08-20 19:44:33 +0000

    E11

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,15 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+var km =parseInt(total);
+
+if (km > 100) {
+    console.log("We still have a bit of driving left to go");
+    console.log(km);
+  } else if (km > 50) {
+    console.log("We'll be there in 5 minutes");
+    console.log(km);
+  }else{
+    console.log("I'm parking, I'll see you right now");
+    console.log(km);
+  }
