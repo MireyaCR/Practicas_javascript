3dbc9565debeb9131bfa48baf1c01f60d059f6dc
MireyaCR <mireya.mcr@gmail.com>
2022-08-20 18:48:34 +0000

    E10

--- .learn/resets/10-Create_a_new_function/app.js
+++ .learn/resets/10-Create_a_new_function/app.js
@@ -1,9 +1,3 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
 // Declare your function here
-var x;
-function generateRandom (){
-    x= Math.floor(Math.random()*9);
-    return x;
-}
-console.log(generateRandom(x));
