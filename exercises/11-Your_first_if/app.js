be67f6d67ed12b4c47f74f04e231b5a775b3f6ca
MireyaCR <mireya.mcr@gmail.com>
2022-08-21 18:23:57 +0000

    E11.1

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -7,6 +7,6 @@ if (km > 100) {
     console.log("We still have a bit of driving left to go");
 } else if (km > 50) {
     console.log("We'll be there in 5 minutes");
-    }else{
-    console.log("I'm parking, I'll see you right now");
-   }
+} else {
+    console.log("I'm parking. I'll see you right now");
+}
